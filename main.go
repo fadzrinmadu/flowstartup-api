89b1bd82b4884786f5f94febb7b06bf5a8c87962
Muhammad Fadzrin Madu <muhfadzrinmadu@gmail.com>
2023-12-23 23:11:34 +0700

    init project. register user endpoint

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"flowstartup/handler"
+	"flowstartup/user"
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func main() {
+	dsn := "root:superoot@tcp(127.0.0.1:3306)/db_flowstartup?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	userRepository := user.NewRepository(db)
+	userService := user.NewService(userRepository)
+	
+	userHandler := handler.NewUserHandler(userService)
+	
+	router := gin.Default()
+	api := router.Group("/api/v1")
+	
+	api.POST("/users", userHandler.RegisterUser)
+	
+	router.Run()
+}
